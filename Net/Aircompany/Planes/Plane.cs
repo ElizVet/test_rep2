84c47890ed5664970641d759f654cea7b09da0f1
ElizVet <shimliza64@gmail.com>
2022-11-24 09:17:20 +0300

    cleanCodeForJenkins

---
+++ Net/Aircompany/Planes/Plane.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Aircompany.Planes
+{
+    public abstract class Plane
+    {
+        private string _model;
+        private int _maxSpeed;
+        private int _maxFlightDistance;
+        private int _maxLoadCapacity;
+
+        public Plane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity)
+        {
+            _model = model;
+            _maxSpeed = maxSpeed;
+            _maxFlightDistance = maxFlightDistance;
+            _maxLoadCapacity = maxLoadCapacity;
+        }
+
+        public string GetModel() => _model;
+        
+        public int GetMaxSpeed() => _maxSpeed;
+        
+        public int GetMaxFlightDistance() => _maxFlightDistance;
+        
+        public int GetMaxLoadCapacity() => _maxLoadCapacity;
+        
+        public override string ToString() => "Plane{" +
+                "model='" + _model + '\'' +
+                ", maxSpeed=" + _maxSpeed +
+                ", maxFlightDistance=" + _maxFlightDistance +
+                ", maxLoadCapacity=" + _maxLoadCapacity + '}';
+        
+        public override bool Equals(object obj)
+        {
+            var plane = obj as Plane;
+            return (plane != null) &&
+                   (_model == plane._model) &&
+                   (_maxSpeed == plane._maxSpeed) &&
+                   (_maxFlightDistance == plane._maxFlightDistance) &&
+                   (_maxLoadCapacity == plane._maxLoadCapacity);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1043886837;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_model);
+            hashCode = hashCode * -1521134295 + _maxSpeed.GetHashCode();
+            hashCode = hashCode * -1521134295 + _maxFlightDistance.GetHashCode();
+            hashCode = hashCode * -1521134295 + _maxLoadCapacity.GetHashCode();
+            return hashCode;
+        }        
+
+    }
+}
---
+++ Net/AircompanyTests/Tests/AirportTest.cs
@@ -0,0 +1,38 @@
+﻿using Aircompany;
+using Aircompany.Models;
+using Aircompany.Planes;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AircompanyTests.Tests
+{
+    [TestFixture]
+    public class AirportTest
+    {
+        private List<Plane> _planes = new List<Plane>(ListPlanes.planes);
+
+        private PassengerPlane _planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
+
+        [Test]
+        public void CheckExistsTransportMilitaryPlanes()
+        {
+            List<MilitaryPlane> transportMilitaryPlanes = new Airport(_planes).GetTransportMilitaryPlanes().ToList();
+            Assert.IsTrue(transportMilitaryPlanes.Count > 0);
+        }
+
+        [Test]
+        public void CheckExistsPassengerPlaneWithMaxPassengersCapacity()
+        {
+            PassengerPlane expectedPlaneWithMaxPassengersCapacity = new Airport(_planes).GetPassengerPlaneWithMaxPassengersCapacity();
+            Assert.IsTrue(expectedPlaneWithMaxPassengersCapacity.Equals(_planeWithMaxPassengerCapacity));
+        }
+
+        [Test]
+        public void CheckSortedByMaxLoadCapacity()
+        {
+            List<Plane> planesSortedByMaxLoadCapacity = new Airport(_planes).SortByMaxLoadCapacity().GetPlanes().ToList();
+            Assert.That(planesSortedByMaxLoadCapacity.SequenceEqual(_planes.OrderBy(plane => plane.GetMaxLoadCapacity()).ToList()));
+        }
+    }
+}
